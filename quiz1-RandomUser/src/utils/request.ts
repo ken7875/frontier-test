1160b357771d4e77a1ca071602e56002a216c02e
ken7875 <ken19970723@gmail.com>
2023-05-05 01:15:15 +0800

    刪除不必要資料 修改readme

--- quiz1-RandomUser/src/utils/request.ts
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios'
-
-// export default () => {
-//   const service = axios.create({
-//     baseURL: 'https://randomuser.me/api/'
-//   })
-
-//   service.interceptors.response.use()
-// }
